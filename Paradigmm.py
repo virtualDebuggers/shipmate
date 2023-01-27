0d035d918d004ab8c015d17c78abd0b71005f1a1
virtualDebuggers <123767326+virtualDebuggers@users.noreply.github.com>
2023-01-28 02:19:50 +0530

    Update Paradigmm.py

--- Paradigmm.py
+++ Paradigmm.py
@@ -323,7 +323,11 @@ print('Training score for Random Forest Regressor is',RF.score(x_train,y_train))
 
 
 # In[ ]:
-
-
+import joblib
+joblib.dump(clf, “rf_model.sav”)
+def predict(data):
+    clf = joblib.load("rf_model.sav")
+    return clf.predict(data)
+from prediction import predict
 
 
